6a997953d186a50ed34c759c548eabc3dd11a337
Jürgen Weigert <juergenweigert68@gmail.com>
2026-01-05 18:11:50 +0100

    blink reports bootsel with sleep100ms_bs()

--- src/rp2040/blink/main.c
+++ src/rp2040/blink/main.c
@@ -1,20 +1,90 @@
+#include "stdio.h"
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/sync.h"
+#include "hardware/structs/ioqspi.h"
+#include "hardware/structs/sio.h"
+
+// From pico-examples/common/get_bootsel_button.c
+bool __no_inline_not_in_flash_func(get_bootsel_button)() {
+    const uint CS_PIN_INDEX = 1;
+
+    // Disable interrupts (flash access might be interrupted)
+    uint32_t flags = save_and_disable_interrupts();
+
+    // Float the flash CS pin (Hi-Z)
+    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
+        GPIO_OVERRIDE_LOW << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
+        IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
+
+    // Small delay (can't sleep, no flash access)
+    for (volatile int i = 0; i < 1000; ++i);
+
+    // Read pin state via SIO (button pulls low when pressed)
+    bool button_state = !(sio_hw->gpio_hi_in & (1u << CS_PIN_INDEX));
+
+    // Restore flash CS
+    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
+        GPIO_OVERRIDE_NORMAL << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
+        IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
+
+    restore_interrupts(flags);
+    return button_state;
+}
+
+
+#define LED_PIN 25
+
+#define BLINK_DIT	blink(0)
+#define BLINK_DAH	blink(1)
+
+bool sleep100ms_bs(unsigned n)
+{
+    static bool prev_bootsel_state = 0;
+	bool state = get_bootsel_button();
+
+	for (unsigned i=0; i < n; i++)
+	{
+		sleep_ms(100);
+		bool state = get_bootsel_button();
+		if (state != prev_bootsel_state)
+		{
+			prev_bootsel_state = state;
+			if (state)
+			{
+				if (stdio_usb_connected())
+					printf("BOOTSEL pressed!\n");
+			}
+			else
+			{
+				if (stdio_usb_connected())
+					printf("BOOTSEL released!\n");
+			}
+		}
+	}
+	return state;
+}
+
+int blink(bool dah)
+{
+	gpio_put(LED_PIN, 1);
+	(void)sleep100ms_bs(dah?3:1);
+	gpio_put(LED_PIN, 0);
+	(void)sleep100ms_bs(1);
+}
 
 int main() {
-    const uint LED_PIN = 25;
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+	stdio_init_all();
 
 	// say Hi
-    while (true) {
-        gpio_put(LED_PIN, 1); sleep_ms(100); gpio_put(LED_PIN, 0); sleep_ms(100);
-        gpio_put(LED_PIN, 1); sleep_ms(100); gpio_put(LED_PIN, 0); sleep_ms(100);
-        gpio_put(LED_PIN, 1); sleep_ms(100); gpio_put(LED_PIN, 0); sleep_ms(100);
-        gpio_put(LED_PIN, 1); sleep_ms(100); gpio_put(LED_PIN, 0); sleep_ms(100);
-        sleep_ms(200);
-        gpio_put(LED_PIN, 1); sleep_ms(100); gpio_put(LED_PIN, 0); sleep_ms(100);
-        gpio_put(LED_PIN, 1); sleep_ms(100); gpio_put(LED_PIN, 0); sleep_ms(100);
-        sleep_ms(600);
+    while (true)
+	{
+        BLINK_DIT; BLINK_DIT; BLINK_DIT; BLINK_DIT;
+	    (void)sleep100ms_bs(2);	// total of 3. one already done in the last BLINK_DIT
+        BLINK_DIT; BLINK_DIT;
+	    (void)sleep100ms_bs(6);	// total of 7.
     }
 }
 
